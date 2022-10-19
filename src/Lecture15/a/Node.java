bb9901faa924be381f27b86997a401053e9c6ed5
sghaniunc <sghani@cs.unc.edu>
2022-10-19 12:59:41 -0400

    Lecture15 - ArrayList implementation of Stacks and Queues

---
+++ src/Lecture15/a/Node.java
@@ -0,0 +1,6 @@
+package Lecture15.a;
+
+public class Node<T> {
+    T _value;
+    Node<T> _lowerNode;
+}
---
+++ src/Lecture15/b/Node.java
@@ -0,0 +1,6 @@
+package Lecture15.b;
+
+public class Node<T> {
+    T _value;
+    Node<T> _prevNode;
+}
